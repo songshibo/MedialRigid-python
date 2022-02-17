Fixed CPU-side quaternion to matrix bug & add object info debugging

engine/meshobject.py
@@ -28,13 +28,14 @@ def compute_inertia_const_density(v, f, center):
 
 
 class MeshObject:
-    def __init__(self, name: str, path: str, position, rotation, index, polyscope) -> None:
+    def __init__(self, name: str, path: str, position, rotation, index, polyscope):
         self.name = name
         self.index = index
         v, f = igl.read_triangle_mesh(path)
         self.mesh = polyscope.register_surface_mesh(self.name, v, f)
         self.position = position
         self.orientation = euler_to_quaternion(rotation)
+        print(self.orientation)
         self.scale = self.mesh.get_transform().diagonal()[:3]
         self.TRS = identity()
         self.compute_transform()
@@ -42,6 +43,69 @@ class MeshObject:
 
         self.mass_center = compute_mass_center(v)
         self.inertia = compute_inertia_const_density(v, f, self.mass_center)
+
+        # Medial Axis Transform info
+        self.ma_verts = None
+        self.ma_edges = None
+        self.ma_faces = None
+
+    def load_ma(self, filepath):
+        file = open(filepath, 'r')
+        first_line = file.readline().rstrip()
+        vcount, ecount, fcount = [int(x) for x in first_line.split()]
+        assert vcount != 0, "No Medial Vertices!"
+        # line number
+        lineno = 1
+
+        verts, faces, edges = [], [], []
+
+        # read vertices
+        i = 0
+        while i < vcount:
+            line = file.readline()
+            # skip empty line or comment line
+            if line.isspace() or line[0] == '#':
+                lineno += 1
+                continue
+            v = line.split()
+            # Exception
+            assert v[0] == 'v', "vertex line: {} should start with \'v\'!".format(
+                str(lineno))
+            x, y, z, r = float(v[1]), float(v[2]), float(v[3]), float(v[4])
+            verts.append(np.array([x, y, z, r]))
+            lineno += 1
+            i += 1
+        # read edges
+        i = 0
+        while i < ecount:
+            line = file.readline()
+            if line.isspace() or line[0] == '#':
+                lineno += 1
+                continue
+            e = line.split()
+            # Exception
+            assert e[0] == 'e', "Edge line: {} should start with \'e\'!".format(
+                str(lineno))
+            edges.append(np.array([int(e[1]), int(e[2])]))
+            lineno += 1
+            i += 1
+        # read faces
+        i = 0
+        while i < fcount:
+            line = file.readline()
+            if line.isspace() or line[0] == '#':
+                lineno += 1
+                continue
+            f = line.split()
+            # Exception
+            assert f[0] == 'f', "Face line: {} should start with \'f\'!".format(
+                str(lineno))
+            faces.append(np.array([int(f[1]), int(f[2]), int(f[3])]))
+            lineno += 1
+            i += 1
+
+        self.ma_verts, self.ma_edges, self.ma_faces = np.array(verts), np.array(
+            edges, dtype=np.uint8), np.array(faces, dtype=np.uint8)
 
     def compute_transform(self):
         self.TRS = TRS(self.position, self.orientation, self.scale)

engine/simulator.py
@@ -11,7 +11,7 @@ mat3 = ti.types.matrix(3, 3, ti.f32)
 
 @ti.data_oriented
 class Simulator:
-    def __init__(self, dt, objects) -> None:
+    def __init__(self, dt, objects):
         self.dt = dt
         self.n = len(objects)
         rigidbodies_dict = self.prepare_data(
@@ -47,7 +47,7 @@ class Simulator:
             v.append(np.array([0, 0, 0]))
             omega.append(np.array([0, 0, 0]))
             F.append(np.array([0, 0, 0]))
-            T.append(np.array([100, 0, 0]))
+            T.append(np.array([0, 0, 0]))
         # here dtype is neccessary on Metal, Metal is only-support 32-bit data
         return {
             "pos": np.array(pos, dtype=np.float32),

main.py
@@ -1,4 +1,3 @@
-from matplotlib import backends
 import polyscope as ps
 import polyscope.imgui as psimgui
 import os
@@ -11,6 +10,7 @@ ti.init(arch=ti.gpu, debug=True)
 
 dt = 0.01
 objects = []  # all active objects in scenes
+selected = -1
 
 root_path = os.getcwd()
 ps_config = read_configs(
@@ -42,8 +42,9 @@ for obj_name in scene_config.sections():
     path = scene_config[obj_name]['path']
     position = read_np_array(scene_config[obj_name]['position'], np.float32)
     rotation = read_np_array(scene_config[obj_name]['rotation'], np.float32)
-    objects.append(MeshObject(obj_name, path, position,
-                   rotation, len(objects), ps))
+    obj = MeshObject(obj_name, path, position, rotation, len(objects), ps)
+    obj.load_ma(scene_config[obj_name]['ma_path'])
+    objects.append(obj)
 
 sim = Simulator(dt, objects)
 
@@ -59,6 +60,24 @@ def callback():
             obj.update_transform(result)
 
     psimgui.PopItemWidth()
+
+    # debug selected objects
+    if ps.get_selection()[0] != '':
+        psimgui.Separator()
+        global selected
+        name = ps.get_selection()[0]
+        # update selection
+        if selected == -1 or objects[selected].name != name:
+            for idx, obj in enumerate(objects):
+                if obj.name == name:
+                    selected = idx
+                    break
+        # ui for debugging
+        psimgui.PushItemWidth(200)
+        psimgui.InputFloat3("Position", objects[selected].position)
+        psimgui.InputFloat4("Orientation", objects[selected].orientation)
+        psimgui.InputFloat3("Scale", objects[selected].scale)
+        psimgui.PopItemWidth()
 
 
 ps.init()

util/matrix.py
@@ -39,7 +39,9 @@ def TRS(_position, _orientation, _scale):
     T = translate(_position)
     # Quaternions w+ix+jy+kz are represented as [w,x,y,z] in transformations
     # while we use [x,y,z,w], so here we need a simple swap to make sure orientation is in [w,x,y,z]
-    _orientation[1:4], _orientation[0] = _orientation[:3], _orientation[3]
-    R = quaternion_matrix(_orientation)
+    # _orientation[1:4], _orientation[0] = _orientation[:3], _orientation[3]
+    q = np.array([_orientation[3], _orientation[0],
+                 _orientation[1], _orientation[2]])
+    R = quaternion_matrix(q)
     S = scale(_scale)
     return concatenate_matrices(T, R, S)