26ca42d96b10476674e4529b1ec819c738838ee8
songshibo <stereo.bingo@gmail.com>
2022-02-19 15:28:08 +0800

    add two-type of loops

--- parallel_test.py
+++ parallel_test.py
@@ -4,10 +4,11 @@ import numpy as np
 ti.init(arch=ti.gpu, kernel_profiler=True)
 
 A_np = np.random.rand(1000, 3)
-B_np = np.random.rand(1000, 3)
-C_np = np.random.rand(1000, 3)
+B_np = np.random.rand(2000, 3)
+C_np = np.random.rand(1500, 3)
 
-index_range = np.array([[0, 999], [1000, 1999], [2000, 2999]]).astype(np.int32)
+index_range = np.array(
+    [[0, 1000], [1000, 3000], [3000, 4500]]).astype(np.int32)
 
 total = np.concatenate((A_np, B_np, C_np), axis=0).astype(np.float32)
 
@@ -24,12 +25,12 @@ idx_range.from_numpy(index_range)
 @ti.kernel
 def test1():
     valid_num = 0
-    for x, y in ti.ndrange(3000, 3000):
+    for x, y in ti.ndrange(4500, 4500):
         in_ignore_area = (x >= y)
         for i in ti.static(range(3)):
             low = idx_range[i, 0]
             high = idx_range[i, 1]
-            in_area = (low <= x <= high) and (low <= y <= high)
+            in_area = (low <= x < high) and (low <= y < high)
             in_ignore_area = in_ignore_area or in_area
         if not in_ignore_area:
             ti.atomic_add(valid_num, 1)
@@ -37,5 +38,15 @@ def test1():
     print(valid_num)
 
 
-test1()
+@ti.kernel
+def test2(x_l: ti.int32, x_h: ti.int32, y_l: ti.int32, y_h: ti.int32):
+    valid_num = 0
+    for x, y in ti.ndrange((x_l, x_h), (y_l, y_h)):
+        ti.atomic_add(valid_num, 1)
+    print(valid_num)
+
+
+# test1()
+test2(0, 1000, 1000, 4500)
+test2(1000, 3000, 3000, 4500)
 ti.print_kernel_profile_info()
