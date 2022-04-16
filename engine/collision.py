99e56e0dc762ae23c19c8c0dccfadc764da2bdfd
songshibo <stereo.bingo@gmail.com>
2022-04-16 14:27:13 +0800

    ti.experimental.real_func test

--- engine/collision.py
+++ engine/collision.py
@@ -1,4 +1,5 @@
 import taichi as ti
+import taichi.math as tm
 import taichi_glsl as ts
 
 
@@ -664,94 +665,6 @@ def find_cloeset_t(P1, P2, P3, Sr):
     else:
         t = ts.clamp((-P2 - ti.sqrt(delta)) / (2.0 * P1), 0.0, 1.0)
     return t, delta
-
-
-@ti.func
-def slab_slab_cond1(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, R1, R2, R3, R4):
-    t1, t2, t3, t4 = -1.0, -1.0, -1.0, -1.0
-    K1_ = 2.0*A1*R2 - A5 * R1
-    K2_ = A6 * R2 - A8 * R1
-    K3_ = A7 * R2 - A9 * R1
-    K4_ = A11 * R2 - A12 * R1
-    K5_ = A5 * R2 - 2.0*A2*R1
-
-    if K5_ != 0.0:
-        K1_ = -K1_ / K5_
-        K2_ = -K2_ / K5_
-        K3_ = -K3_ / K5_
-        K4_ = -K4_ / K5_
-        H1_ = (2.0*A1 + A5 * K1_)*R3 - (A6 + A8 * K1_)*R1
-        H2_ = (A7 + A5 * K3_)*R3 - (A10 + A8 * K3_)*R1
-        H3_ = (A11 + A5 * K4_)*R3 - (A13 + A8 * K4_)*R1
-        H4_ = (A6 + A5 * K2_)*R3 - (2.0*A3 + A8 * K2_)*R1
-
-        if H4_ != 0.0:
-            H1_ = -H1_ / H4_
-            H2_ = -H2_ / H4_
-            H3_ = -H3_ / H4_
-            G1 = (2.0*A1 + A5 * K1_ + (A6 + A5 * K2_)*H1_) * \
-                R4 - (A7 + A9 * K1_ + (A10 + A9 * K2_)*H1_)*R1
-            G2 = (A11 + A5 * K4_ + (A6 + A5 * K2_)*H3_)*R4 - \
-                (A14 + A9 * K4_ + (A10 + A9 * K2_)*H3_)*R1
-            G3 = (A7 + A5 * K3_ + (A6 + A5 * K2_)*H2_)*R4 - \
-                (2.0*A4 + A9 * K3_ + (A10 + A9 * K2_)*H2_)*R1
-
-            if G3 != 0.0:
-                G1 = -1.0*G1 / G3
-                G2 = -1.0*G2 / G3
-                K1 = K1_ + K2_ * H1_ + K2_ * H2_*G1 + K3_ * G1
-                K2 = K4_ + K2_ * H2_*G2 + K2_ * H3_ + K3_ * G2
-                H1 = H1_ + H2_ * G1
-                H2 = H3_ + H2_ * G2
-
-                # W1 t1^2 + W2 t1 + W3 = 0
-                W1 = pow((2.0*A1 + A5 * K1 + A6 * H1 + A7 * G1), 2.0) - 4.0*R1*R1*(A1 + A2 * K1*K1 + A3 *
-                                                                                   H1*H1 + A4 * G1*G1 + A5 * K1 + A6 * H1 + A7 * G1 + A8 * K1*H1 + A9 * K1*G1 + A10 * H1*G1)
-                W2 = 2.0*(2.0*A1 + A5 * K1 + A6 * H1 + A7 * G1)*(A11 + A5 * K2 + A6 * H2 + A7 * G2) - 4.0*R1*R1*(2.0*A2*K1*K2 + 2.0*A3*H1*H2 + 2.0*A4*G1*G2 + A5 *
-                                                                                                                 K2 + A6 * H2 + A7 * G2 + A8 * K1*H2 + A8 * K2*H1 + A9 * K1*G2 + A9 * K2*G1 + A10 * H1*G2 + A10 * H2*G1 + A11 + A12 * K1 + A13 * H1 + A14 * G1)
-                W3 = pow((A11 + A5 * K2 + A6 * H2 + A7 * G2), 2.0) - 4.0*R1*R1*(A2*K2*K2 + A3 * H2*H2 +
-                                                                                A4 * G2*G2 + A8 * K2*H2 + A9 * K2*G2 + A10 * H2*G2 + A12 * K2 + A13 * H2 + A14 * G2 + A15)
-                det = max(W2 * W2 - 4.0*W1*W3, 0.0)
-
-                if not (W1 == 0.0 and W2 == 0.0 and W3 == 0.0):
-                    if W1 != 0.0:
-                        t1 = (-1.0 * W2 - ti.sqrt(det)) / (2.0*W1)
-                    elif W2 != 0.0:
-                        t1 = -1.0 * W3 / W2
-                    else:
-                        t1 = -1.0
-                    t2 = K1 * t1 + K2
-                    t3 = H1 * t1 + H2
-                    t4 = G1 * t1 + G2
-            else:
-                L1 = 0.0
-                L2 = -1.0*G2 / G1
-                K1 = K1_ * L1 + K2_ * H1_ + K2_ * L1*H2_ + K3_
-                K2 = K1_ * L2 + K2_ * H2_*L2 + K2_ * H3_ + K4_
-                H1 = H1_ + L1 * H2_
-                H2 = H3_ + L2 * H2_
-                # W1 t4^2 + W2 t4 + W3 = 0
-                W1 = pow((A6*L1 + A8 * K1 + 2.0*A3*H1 + A10), 2.0) - 4.0*R4*R4*(A1*L1*L1 + A2 * K1*K1 +
-                                                                                A3 * H1*H1 + A4 + A5 * L1*K1 + A6 * L1*H1 + A7 * L1 + A8 * K1*H1 + A9 * K1 + A10 * H1)
-                W2 = 2.0*(A6*L1 + A8 * K1 + 2.0*A3*H1 + A10)*(A6*L2 + A8 * K2 + 2.0*A3*H2 + A13) - 4.0*R4*R4*(2.0*A1*L1*L2 + 2.0*A2*K1*K2 + 2.0*A3*H1*H2 + A5 *
-                                                                                                              L1*K2 + A5 * L2*K1 + A6 * L1*H2 + A6 * L2*H1 + A7 * L2 + A8 * K1*H2 + A8 * K2*H1 + A9 * K2 + A10 * H2 + A11 * L1 + A12 * K1 + A13 * H1 + A14)
-                W3 = pow((A6*L2 + A8 * K2 + 2.0*A3*H2 + A13), 2.0) - 4.0*R4*R4*(A1*L2*L2 + A2 * K2*K2 +
-                                                                                A3 * H2*H2 + A5 * L2*K2 + A6 * L2*H2 + A8 * K2*H2 + A11 * L2 + A12 * K2 + A13 * H2 + A15)
-                det = max(W2 * W2 - 4.0*W1*W3, 0.0)
-
-                if not (W1 == 0.0 and W2 == 0.0 and W3 == 0.0):
-                    if W1 != 0.0:
-                        t4 = (-1.0*W2 - ti.sqrt(det)) / (2.0*W1)
-                    elif W2 != 0.0:
-                        t4 = -1.0*W3 / W2
-                    else:
-                        t4 = -1.0
-
-                    t1 = L1 * t4 + L2
-                    t2 = K1 * t4 + K2
-                    t3 = H1 * t4 + H2
-        else:
-            pass
 
 
 @ti.func
--- manually.py
+++ manually.py
@@ -1,12 +1,18 @@
 import numpy as np
 import taichi as ti
 import taichi_glsl as ts
+import taichi.math as tm
 
 ti.init()
 
 
 @ti.kernel
 def surface_distane(c1: ti.template(), c2: ti.template(), r1: ti.f32, r2: ti.f32) -> ti.f32:
+    return tm.distance(c1, c2) - (r1+r2)
+
+
+@ti.kernel
+def surface_distane_glsl(c1: ti.template(), c2: ti.template(), r1: ti.f32, r2: ti.f32) -> ti.f32:
     return ts.length(c1-c2) - (r1 + r2)
 
 
@@ -16,73 +22,76 @@ v11 = ti.Vector([0.0, -1, 0.0])
 c12 = ti.Vector([0.0, 1.0, 0.0])
 r12 = 0.45
 v12 = ti.Vector([0.0, -0.5, 0.3])
-
-c21 = ti.Vector([0.5, 0.0, 0.0])
-r21 = 0.35
-v21 = ti.Vector([0.0, 0.0, 0.0])
-c22 = ti.Vector([-0.5, 0.0, 0.0])
-r22 = 0.5
-v22 = ti.Vector([0.0, 0.0, 0.0])
-c23 = ti.Vector([0.0, 0.0, -0.6])
-r23 = 0.2
-v23 = ti.Vector([0.0, 0.0, 0.0])
+print(surface_distane(c11, c12, r11, r12))
+print(surface_distane_glsl(c11, c12, r11, r12))
 
 
-c11c12 = c11-c12
-c21c23 = c21-c23
-c22c23 = c22-c23
-c12c23 = c12-c23
-v11v12 = v11-v12
-v21v23 = v21-v23
-v22v23 = v22-v23
-v12v23 = v12-v23
+# c21 = ti.Vector([0.5, 0.0, 0.0])
+# r21 = 0.35
+# v21 = ti.Vector([0.0, 0.0, 0.0])
+# c22 = ti.Vector([-0.5, 0.0, 0.0])
+# r22 = 0.5
+# v22 = ti.Vector([0.0, 0.0, 0.0])
+# c23 = ti.Vector([0.0, 0.0, -0.6])
+# r23 = 0.2
+# v23 = ti.Vector([0.0, 0.0, 0.0])
 
-A1 = ts.sqrLength(v11v12)
-A2 = 2.0 * ts.dot(v11v12, c11c12)
-A3 = ts.sqrLength(c11c12)
-# y^2
-B1 = ts.sqrLength(v21v23)
-B2 = 2.0 * ts.dot(v21v23, c21c23)
-B3 = ts.sqrLength(c21c23)
-# z^2
-C1 = ts.sqrLength(v22v23)
-C2 = 2.0 * ts.dot(v22v23, c22c23)
-C3 = ts.sqrLength(c22c23)
-# xy
-D1 = -2.0 * ts.dot(v11v12, v21v23)
-D2 = -2.0 * (ts.dot(v11v12, c21c23) + ts.dot(c11c12, v21v23))
-D3 = -2.0 * ts.dot(c11c12, c21c23)
-# xz
-E1 = -2.0 * ts.dot(v11v12, v22v23)
-E2 = -2.0 * (ts.dot(v11v12, c22c23) + ts.dot(c11c12, v22v23))
-E3 = -2.0 * ts.dot(c11c12, c22c23)
-# yz
-F1 = 2.0 * ts.dot(v21v23, v22v23)
-F2 = 2.0 * (ts.dot(v21v23, c22c23) + ts.dot(c21c23, v22v23))
-F3 = 2.0 * ts.dot(c21c23, c22c23)
-# x
-G1 = 2.0 * ts.dot(v11v12, v12v23)
-G2 = 2.0 * (ts.dot(v11v12, c12c23) + ts.dot(c11c12, v12v23))
-G3 = 2.0 * ts.dot(c11c12, c12c23)
-# y
-H1 = -2.0 * ts.dot(v21v23, v12v23)
-H2 = -2.0 * (ts.dot(v21v23, c12c23) + ts.dot(c21c23, v12v23))
-H3 = -2.0 * ts.dot(c21c23, c12c23)
-# z
-I1 = -2.0 * ts.dot(v22v23, v12v23)
-I2 = -2.0 * (ts.dot(v22v23, c12c23) + ts.dot(v12v23, c22c23))
-I3 = -2.0 * ts.dot(c22c23, c12c23)
-# t
-J1 = ts.sqrLength(v12v23)
-J2 = 2.0 * ts.dot(v12v23, c12c23)
-J3 = ts.sqrLength(c12c23)
-R1, R2, R3, R4 = r11-r12, r21-r23, r22-r23, r12+r23
 
-x, y, z, t = 0.0, 1.0, 0.0, 0.0
-cx = (c11 + v11 * t) * x + (c12 + v12 * t) * (1.0 - x)
-rx = r11 * x + r12 * (1.0-x)
-cyz = (c21 + v21 * t) * y + (c22 + v22 * t) * z + (c23 + v23 * t) * (1.0-y-z)
-ryz = r21 * y + r22 * z + r23 * (1.0-y-z)
+# c11c12 = c11-c12
+# c21c23 = c21-c23
+# c22c23 = c22-c23
+# c12c23 = c12-c23
+# v11v12 = v11-v12
+# v21v23 = v21-v23
+# v22v23 = v22-v23
+# v12v23 = v12-v23
+
+# A1 = ts.sqrLength(v11v12)
+# A2 = 2.0 * ts.dot(v11v12, c11c12)
+# A3 = ts.sqrLength(c11c12)
+# # y^2
+# B1 = ts.sqrLength(v21v23)
+# B2 = 2.0 * ts.dot(v21v23, c21c23)
+# B3 = ts.sqrLength(c21c23)
+# # z^2
+# C1 = ts.sqrLength(v22v23)
+# C2 = 2.0 * ts.dot(v22v23, c22c23)
+# C3 = ts.sqrLength(c22c23)
+# # xy
+# D1 = -2.0 * ts.dot(v11v12, v21v23)
+# D2 = -2.0 * (ts.dot(v11v12, c21c23) + ts.dot(c11c12, v21v23))
+# D3 = -2.0 * ts.dot(c11c12, c21c23)
+# # xz
+# E1 = -2.0 * ts.dot(v11v12, v22v23)
+# E2 = -2.0 * (ts.dot(v11v12, c22c23) + ts.dot(c11c12, v22v23))
+# E3 = -2.0 * ts.dot(c11c12, c22c23)
+# # yz
+# F1 = 2.0 * ts.dot(v21v23, v22v23)
+# F2 = 2.0 * (ts.dot(v21v23, c22c23) + ts.dot(c21c23, v22v23))
+# F3 = 2.0 * ts.dot(c21c23, c22c23)
+# # x
+# G1 = 2.0 * ts.dot(v11v12, v12v23)
+# G2 = 2.0 * (ts.dot(v11v12, c12c23) + ts.dot(c11c12, v12v23))
+# G3 = 2.0 * ts.dot(c11c12, c12c23)
+# # y
+# H1 = -2.0 * ts.dot(v21v23, v12v23)
+# H2 = -2.0 * (ts.dot(v21v23, c12c23) + ts.dot(c21c23, v12v23))
+# H3 = -2.0 * ts.dot(c21c23, c12c23)
+# # z
+# I1 = -2.0 * ts.dot(v22v23, v12v23)
+# I2 = -2.0 * (ts.dot(v22v23, c12c23) + ts.dot(v12v23, c22c23))
+# I3 = -2.0 * ts.dot(c22c23, c12c23)
+# # t
+# J1 = ts.sqrLength(v12v23)
+# J2 = 2.0 * ts.dot(v12v23, c12c23)
+# J3 = ts.sqrLength(c12c23)
+# R1, R2, R3, R4 = r11-r12, r21-r23, r22-r23, r12+r23
+
+# x, y, z, t = 0.0, 1.0, 0.0, 0.0
+# cx = (c11 + v11 * t) * x + (c12 + v12 * t) * (1.0 - x)
+# rx = r11 * x + r12 * (1.0-x)
+# cyz = (c21 + v21 * t) * y + (c22 + v22 * t) * z + (c23 + v23 * t) * (1.0-y-z)
+# ryz = r21 * y + r22 * z + r23 * (1.0-y-z)
 # print(ts.sqrLength(cx-cyz))
 # print(rx + ryz)
 # print(surface_distane(cx, cyz, rx, ryz))
@@ -94,23 +103,23 @@ ryz = r21 * y + r22 * z + r23 * (1.0-y-z)
 # print(sc)
 # print(R1*x+R2*y+R3*z+R4)
 
-m11 = np.array([0.0, 0.7, 0.5, 0.3])
-m11 = np.array([0.0, 0.7, 0.5, 0.3])
-v11 = np.array([0.0, -1.0, 0.0])
-m12 = np.array([0.0, 1.0, 0.0, 0.35])
-v12 = np.array([0.0, -0.5, 0.3])
-m13 = np.array([0.5, 0.7, -0.4, 0.25])
-v13 = np.array([0.0, -1.0, 0.0])
-# second
-m21 = np.array([0.5, 0.0, 0.0, 0.35])
-v21 = np.array([0.0, 0.0, 0.0])
-m22 = np.array([-0.5, 0.0, 0.0, 0.5])
-v22 = np.array([0.0, 0.0, 0.0])
-m23 = np.array([0.0, 0.0, -0.6, 0.2])
-v23 = np.array([0.0, 0.0, 0.0])
-t1, t2 = 0.7543255686759949, 0.24567443132400513
-t3, t4 = 0.36941203474998474, 0.6305879354476929
-m1 = m11 * t1 + m12 * t2 + m13 * (1-t1-t2)
-m2 = m21 * t3 + m22 * t3 + m23 * (1-t3-t4)
-print(np.linalg.norm(m1[:3]-m2[:3])-m1[3]-m2[3])
+# m11 = np.array([0.0, 0.7, 0.5, 0.3])
+# m11 = np.array([0.0, 0.7, 0.5, 0.3])
+# v11 = np.array([0.0, -1.0, 0.0])
+# m12 = np.array([0.0, 1.0, 0.0, 0.35])
+# v12 = np.array([0.0, -0.5, 0.3])
+# m13 = np.array([0.5, 0.7, -0.4, 0.25])
+# v13 = np.array([0.0, -1.0, 0.0])
+# # second
+# m21 = np.array([0.5, 0.0, 0.0, 0.35])
+# v21 = np.array([0.0, 0.0, 0.0])
+# m22 = np.array([-0.5, 0.0, 0.0, 0.5])
+# v22 = np.array([0.0, 0.0, 0.0])
+# m23 = np.array([0.0, 0.0, -0.6, 0.2])
+# v23 = np.array([0.0, 0.0, 0.0])
+# t1, t2 = 0.7543255686759949, 0.24567443132400513
+# t3, t4 = 0.36941203474998474, 0.6305879354476929
+# m1 = m11 * t1 + m12 * t2 + m13 * (1-t1-t2)
+# m2 = m21 * t3 + m22 * t3 + m23 * (1-t3-t4)
+# print(np.linalg.norm(m1[:3]-m2[:3])-m1[3]-m2[3])
 # print(m1[3]+m2[3])
